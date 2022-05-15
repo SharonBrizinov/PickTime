1005830d34b77cfdb03f42632bb7550a44d19c8f
Sharon Brizinov <SharonBrizinov@users.noreply.github.com>
2022-05-15 18:47:26 +0300

    added real/anonymous tokens

--- content.js
+++ content.js
@@ -1,22 +1,41 @@
-var isDebug = false;
+var isDebug = true;
 var intervalTime = 9 * 1000; // 9 seconds
+var isAllowSendRealToken = true; // Allow to send non-anonymous too?
 
 function checkPickTimeToken() {
   if (isDebug)
-    console.log("checkPickTimeToken start");
+    console.log("PickTime: checkPickTimeToken start");
 
   // Try to get user session
-  userToken = sessionStorage.getItem("user.session-token");
-  if (userToken) {
+  userAnonymToken = sessionStorage.getItem("user.session-token");
+  userRealToken = localStorage.getItem("user.tkn");
 
+  // We prefer anonymous token, but will take real token if allowed (isSendAnonymousOnly == false)
+  newToken = "";
+  if (userAnonymToken) {
+    if (isDebug)
+      console.log("PickTime: got anonymous token");
+    newTokenType = "anonymous";
+    newToken = userAnonymToken;
+  }
+  else {
+    if (isAllowSendRealToken)
+      if (userRealToken) {
+        if (isDebug)
+          console.log("PickTime: got real token");
+        newTokenType = "real";
+        newToken = userRealToken;
+      }
+  }
+  
+  if (newToken) {
     // Get previously sent token
-    userTokenSent = localStorage.getItem("picktime_token_sent");
+    previousToken = localStorage.getItem("picktime_token_sent");
 
-    if (!userTokenSent || userTokenSent != userToken) {
-
+    if (previousToken != newToken) {
       console.log("PickTime: time to update token");
 
-      localStorage.setItem("picktime_token_sent", userToken);
+      localStorage.setItem("picktime_token_sent", newToken);
 
       fetch('https://picktimebot.com/auth_token', {
           method: 'POST',
@@ -24,15 +43,18 @@ function checkPickTimeToken() {
             'Accept': 'application/json, text/plain, */*',
             'Content-Type': 'application/json'
         },
-          body: JSON.stringify({token: userToken})
+          body: JSON.stringify({token: newToken, type: newTokenType})
         }).then(res => res.json())
-          .then(res => console.log(res));
+          .then(res => console.log("PickTime: " + res));
 
     }
   }
   if (isDebug)
-    console.log("checkPickTimeToken end");
+    console.log("PickTime: checkPickTimeToken end");
 }
+
+if (isDebug)
+  console.log("PickTime: js installed");
 
 checkPickTimeToken();
 setInterval(checkPickTimeToken, intervalTime);
